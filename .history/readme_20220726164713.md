cc303f507589768125c10d662367c627ed66aa96
ArthurHendrich <arthuralenc@gmail.com>
2022-07-26 16:47:47 -0300

    first commit

---
+++ .history/readme_20220726164713.md
@@ -0,0 +1,34 @@
+# Ignews
+
+<br>
+
+- ReactJS
+- NextJS
+- Typescript
+- SCSS
+- CMS: Prismic
+
+<br><br>
+
+## Desafio!!
+
+- Projeto desenvolvido para um funcionamento de um blog
+
+  <br>
+
+### Instalação
+
+- `yarn`
+
+<br>
+
+## Manual de Uso
+
+<br>
+
+#### Incialize
+
+- `yarn dev`
+  <br>
+
+- `http://localhost:3000/`
---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,73 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react"
+
+import { stripe } from "../../services/stripe";
+
+import { fauna } from "../../services/fauna";
+import { query as q} from 'faunadb';
+
+
+type User = {
+  ref: {
+    id: string;
+  }
+  data: {
+    stripeCostumerId: string
+  }
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const session = await getSession({req});
+
+    const user = await fauna.query<User>(
+      q.Get(
+        q.Match(
+          q.Index('user_by_email'),
+          q.Casefold(session.user.email)
+        )
+      )
+    ) 
+
+    // verify if user exists
+    let costumerId = user.data.stripeCostumerId;
+
+    if (!costumerId) {
+      const stripeCostumer = await stripe.customers.create({
+        email: session.user.email,
+      })
+
+      await fauna.query(
+        q.Update(
+          q.Ref(q.Collection('users'), user.ref.id),
+          {
+            data: {
+              stripeCostumerId: stripeCostumer.id,
+            }
+          }
+        )
+      )
+
+      costumerId = stripeCostumer.id
+    }
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: costumerId, 
+      payment_method_types: ['card'],
+      billing_address_collection: 'required',
+      line_items: [
+        {price: 'price_1LHHxsI5v5ABKILNMhKU0zPO', quantity: 1}
+      ],
+      mode: 'subscription',
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL
+    })
+    
+    return res.status(200).json({sessionId: stripeCheckoutSession.id})
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end('Method not allowrd')
+  }
+}
